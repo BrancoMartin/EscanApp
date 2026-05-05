89b5af539870863418774b666441065722fa7abd
Branco <brancoantu9@gmail.com>
2026-05-05 15:58:54 -0300

    5-5: saque la validacion de la descripcion

--- Frontend/src/Components/AddProduct/Validation.jsx
+++ Frontend/src/Components/AddProduct/Validation.jsx
@@ -2,7 +2,7 @@ const addProductValidation = ({ form }) => {
   console.log("FORMULARIO ENTRANDO AL ADD PRODUCT", form);
   const errors = {};
 
-  const names = ["barcode", "name", "price", "description"];
+  const names = ["barcode", "name", "price"];
   for (const n of names) {
     if (!form[n].trim()) {
       errors[n] = `* debe ingresar el ${n}`;
