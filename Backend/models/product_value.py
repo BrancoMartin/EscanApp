c8ed48265dc212bbc68d7be6e477dc835d5d4ffb
Branco <brancoantu9@gmail.com>
2026-05-31 20:51:47 -0300

    corregi el extend_existing del product_value

--- Backend/models/product_value.py
+++ Backend/models/product_value.py
@@ -2,18 +2,15 @@ from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from Backend.database import Base
 
-
 class ProductValue(Base):
     __tablename__ = "product_values"
-    __table_args__ = {"extend_existing": True}
-
+    __table_args__ = (
+        UniqueConstraint("product_id", "value_id"),
+        {"extend_existing": True}
+    )
     id = Column(Integer, primary_key=True, autoincrement=True)
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     value_id = Column(Integer, ForeignKey("values.id"), nullable=False)
-
-    __table_args__ = (
-        UniqueConstraint("product_id", "value_id"),
-    )
 
     product = relationship("Product", back_populates="product_values")
     value = relationship("Value", back_populates="product_values")
