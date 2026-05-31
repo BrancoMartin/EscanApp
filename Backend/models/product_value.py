07d0d4a7f85f65d8010cb7e71497cc85a4d06ddb
Branco <brancoantu9@gmail.com>
2026-05-31 20:11:36 -0300

    31-5: puse backend de nuevo adelante de otra importacion

--- Backend/models/product_value.py
+++ Backend/models/product_value.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
-from database import Base
+from Backend.database import Base
 
 
 class ProductValue(Base):
