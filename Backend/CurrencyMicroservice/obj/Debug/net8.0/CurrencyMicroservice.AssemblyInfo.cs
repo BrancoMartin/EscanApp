d7512be63d58d09d0e649782c65abab4db90d14a
Branco <brancoantu9@gmail.com>
2026-07-06 11:44:10 -0300

    cambios en el microservicio currency, no se por que se generan

--- Backend/CurrencyMicroservice/obj/Debug/net8.0/CurrencyMicroservice.AssemblyInfo.cs
+++ Backend/CurrencyMicroservice/obj/Debug/net8.0/CurrencyMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6010f80eeb63a19694d82febb519a1d6350ebbed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef624eb09b3cec6e438643ed7d278e04375e71a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
